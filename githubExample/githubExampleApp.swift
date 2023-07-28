e2c64db5a84ed3d6fbaecc91945b733fe3329a34
Aayushi Garg <aayushigarg17@gmail.com>
2023-07-28 10:05:51 -0700

    Initial Commit

---
+++ githubExample/githubExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  githubExampleApp.swift
+//  githubExample
+//
+//  Created by Aayushi Garg on 7/28/23.
+//
+
+import SwiftUI
+
+@main
+struct githubExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
